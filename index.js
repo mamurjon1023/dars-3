224e286197ef644331d64a7eee2f350cdea7f74b
mamurjon1148 <mamurjonovmamurjon01@gmail.com>
2024-05-25 15:03:24 +0500

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+let randomson = Math.floor(Math.random()*1000);
+
+console.log(randomson)
+
+let son = Number(prompt("sonni kiriting"));
+alert(randomson+son);
+alert(randomson-son);
+alert(randomson*son);
+alert(randomson/son);
